d471de014b73f7f6960aaa0ca35fd286637c1798
janani0212 <janani@madecomfy.com.au>
2022-12-21 18:23:54 +1100

    feedback updates

--- public/script/modal.js
+++ public/script/modal.js
@@ -6,7 +6,7 @@ $(".open_popup").click(function () {
 	$(".plus").addClass("opacity");
 	$("body").addClass("overflow");
 });
-$(".popup_close").click(function () {
+$(".popup_close, .popup_cancel").click(function () {
 	$(".popup_body").removeClass("popup_body_show");
 	$(".plus").removeClass("opacity");
 	$("body").removeClass("overflow");
